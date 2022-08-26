64298d5a84043d5dedb373d69cfefc77a8a684f3
sxs <2073868401@qq.com>
2022-08-26 16:24:14 +0800

    Initial commit

---
+++ src/main/java/com/sxs/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.sxs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sxs
+ * @create 2022-08-26 15:57
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        System.out.println("hello world...");
+        return "ok";
+    }
+}
